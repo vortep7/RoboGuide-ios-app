afa8f676171d496e3d7f79f4851e9344794ac24a
vortep.7 <0ndreyp7777@gmail.com>
2024-02-29 18:05:27 +0300

    Initial Commit

---
+++ MechatronicsComponentsGuide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MechatronicsComponentsGuide
+//
+//  Created by Андрей Петров on 29.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
