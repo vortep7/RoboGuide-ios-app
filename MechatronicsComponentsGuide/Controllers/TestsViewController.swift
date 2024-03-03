63ec203ff02f153434d6bd5b711326b4fc5b9d4c
vortep.7 <0ndreyp7777@gmail.com>
2024-03-03 09:09:34 +0300

    create aanswers label

--- MechatronicsComponentsGuide/Controllers/TestsViewController.swift
+++ MechatronicsComponentsGuide/Controllers/TestsViewController.swift
@@ -19,10 +19,24 @@ class TestsViewController: UIViewController {
     @objc func actionForButton() {
         if test.digit < 5 {
             test.digit += 1
+            JSONParcer.shared.fetchData()
+            print(JSONParcer.shared.arrayOfText)
+            print(JSONParcer.shared.arrayOfAnswer)
             test.label.text = JSONParcer.shared.arrayOfText[test.digit]
+            
+            test.firstAnswer.text = JSONParcer.shared.arrayOfAnswer[test.digit][0]
+            test.secondAnswer.text = JSONParcer.shared.arrayOfAnswer[test.digit][1]
+            test.thirdAnswer.text = JSONParcer.shared.arrayOfAnswer[test.digit][2]
+            test.fouthrAnswer.text = JSONParcer.shared.arrayOfAnswer[test.digit][3]
+            
+            test.qualityOfLevel.text = JSONParcer.shared.arrayOfLevel[test.digit]
         } else {
             test.digit = 0
             test.label.text = JSONParcer.shared.arrayOfText[test.digit]
+            test.firstAnswer.text = JSONParcer.shared.arrayOfAnswer[test.digit][0]
+            test.secondAnswer.text = JSONParcer.shared.arrayOfAnswer[test.digit][1]
+            test.thirdAnswer.text = JSONParcer.shared.arrayOfAnswer[test.digit][2]
+            test.fouthrAnswer.text = JSONParcer.shared.arrayOfAnswer[test.digit][3]
         }
     }
 }
--- MechatronicsComponentsGuide/Views/TestsView.swift
+++ MechatronicsComponentsGuide/Views/TestsView.swift
@@ -23,13 +23,61 @@ final class TestsView: UIView {
     
     let label: UILabel = {
         let label = UILabel()
-        JSONParcer.shared.fetchData()
+        label.text = Words.firstLabel.rawValue
+        label.font = .boldSystemFont(ofSize: 24)
+        label.numberOfLines = 5
+        return label
+    }()
+    
+    //MARK: - create answers
+    var firstAnswer: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 18)
+        label.textColor = .orange
+        return label
+    }()
+    
+    var secondAnswer: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 18)
+        label.textColor = .orange
+        return label
+    }()
+    
+    var thirdAnswer: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 18)
+        label.textColor = .orange
+        return label
+    }()
+    
+    var fouthrAnswer: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 18)
+        label.textColor = .orange
+        return label
+    }()
+    
+    var qualityOfLevel: UILabel = {
+        let label = UILabel()
+//        label.font = .boldSystemFont(ofSize: 32)
+        label.font = UIFont(name: "abosanova", size: 35)
+        label.textColor = .white
         return label
     }()
     
     private let button:UIButton = {
         let button = UIButton()
         button.setTitle("Next question", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.backgroundColor = .white
+        button.layer.cornerRadius = 18
+        return button
+    }()
+    
+    private let hintButton:UIButton = {
+        let button = UIButton()
+        button.setTitle("Hint", for: .normal)
         button.setTitleColor(.blue, for: .normal)
         button.backgroundColor = .white
         button.layer.cornerRadius = 18
@@ -46,6 +94,16 @@ final class TestsView: UIView {
         ])
     }
     
+    func constraintsFotQualityOfLevel() {
+        qualityOfLevel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            qualityOfLevel.topAnchor.constraint(equalTo: self.topAnchor, constant: 80),
+            qualityOfLevel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -650),
+            qualityOfLevel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 70),
+            qualityOfLevel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -70),
+        ])
+    }
+    
     func constraintsFotImageViewMini() {
         imageViewMini.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
@@ -59,20 +117,68 @@ final class TestsView: UIView {
     func constraintsFotButton() {
         button.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-//            button.topAnchor.constraint(equalTo: self.topAnchor, constant: 0),
             button.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -100),
             button.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 50),
             button.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -50),
         ])
     }
     
+    func constraintsFotHintButton() {
+        hintButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            hintButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -40),
+            hintButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 50),
+            hintButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -50),
+        ])
+    }
+    
     func constraintsFotLabel() {
         label.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            label.topAnchor.constraint(equalTo: self.topAnchor, constant: 190),
-            label.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -510),
-            label.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
+            label.topAnchor.constraint(equalTo: self.topAnchor, constant: 200),
+            label.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -500),
+            label.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 30),
             label.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+        ])
+    }
+    
+    func constraintsForFirstAnswer() {
+        firstAnswer.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            firstAnswer.topAnchor.constraint(equalTo: self.topAnchor, constant: 300),
+            firstAnswer.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -450),
+            firstAnswer.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
+            firstAnswer.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+        ])
+    }
+    
+    func constraintsForSecondAnswer() {
+        secondAnswer.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            secondAnswer.topAnchor.constraint(equalTo: self.topAnchor, constant: 355),
+            secondAnswer.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -390),
+            secondAnswer.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
+            secondAnswer.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+        ])
+    }
+    
+    func constraintsForThirdAnswer() {
+        thirdAnswer.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            thirdAnswer.topAnchor.constraint(equalTo: self.topAnchor, constant: 400),
+            thirdAnswer.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -330),
+            thirdAnswer.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
+            thirdAnswer.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+        ])
+    }
+    
+    func constraintsForFouthrAnswer() {
+        fouthrAnswer.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            fouthrAnswer.topAnchor.constraint(equalTo: self.topAnchor, constant: 460),
+            fouthrAnswer.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -270),
+            fouthrAnswer.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
+            fouthrAnswer.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
         ])
     }
     
@@ -81,6 +187,12 @@ final class TestsView: UIView {
         constraintsFotImageViewMini()
         constraintsFotLabel()
         constraintsFotButton()
+        constraintsForFirstAnswer()
+        constraintsForSecondAnswer()
+        constraintsForThirdAnswer()
+        constraintsForFouthrAnswer()
+        constraintsFotHintButton()
+        constraintsFotQualityOfLevel()
     }
     
     func addViews() {
@@ -88,6 +200,12 @@ final class TestsView: UIView {
         self.addSubview(imageViewMini)
         self.addSubview(label)
         self.addSubview(button)
+        self.addSubview(firstAnswer)
+        self.addSubview(secondAnswer)
+        self.addSubview(thirdAnswer)
+        self.addSubview(fouthrAnswer)
+        self.addSubview(hintButton)
+        self.addSubview(qualityOfLevel)
     }
     
     func addAction() {
@@ -100,7 +218,6 @@ final class TestsView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        label.text = JSONParcer.shared.arrayOfText[digit]
 
         addViews()
         createConstraints()
@@ -111,4 +228,8 @@ final class TestsView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+}
+
+enum Words: String {
+    case firstLabel = "This is funny RoboTest! Choose the correct answer from among the suggested ones. Points are awarded for correct answers. Good luck, young roboticist!"
 }
